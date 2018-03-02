d8a5e00da22db3b53e74bec29fac091ba8eb74d5
yMk4 <35229372+yMk4@users.noreply.github.com>
2018-03-02 20:31:55 +0200

    Create SafeMath.sol

---
+++ SafeMath.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.19;
+
+library SafeMath {
+    
+    function mul(uint a, uint b) internal pure returns (uint) {
+        if (a == 0) {
+            return 0;
+        }
+    
+        uint c = a * b;
+            assert(c / a == b);
+            return c;
+        }
+    }
+    
+    function div(uint a, uint256 b) internal pure returns (uint) {
+        uint c = a / b;
+        return c;
+    }
+    
+    function sub(uint a, uint b) internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+    
+    function add(uint a, uint b) internal pure returns (uint) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
